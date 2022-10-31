52800e9088bae354dbe90fcf3a33b7568f54a82a
root <jchacon@uccs.edu>
2022-10-31 04:50:29 +0000

    Fixed testing issues :)

--- spec/controller/projects_spec.rb
+++ spec/controller/projects_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe ProjectsController, type: :controller do
   context "GET #index" do
     it "returns a success response" do
       get :index
-      # expect(response.success).to eq(true)
-      expect(response).to be_success
+      #expect(response.successful).to eq(true)
+      expect(response).to be_successful
     end
   end
 
@@ -13,7 +13,7 @@ RSpec.describe ProjectsController, type: :controller do
     let!(:project) { Project.create(title: "Test title", description: "Test description") }
     it "returns a success response" do
       get :show, params: { id: project }
-      expect(response).to be_success
+      expect(response).to be_successful
     end
   end
 end
